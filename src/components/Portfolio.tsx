304f64fcc71cf7d0e73f4a10dd4a766a7d087f4a
Matthew Conrad <matthew.conrad@outlook.com>
2019-09-03 00:51:26 -0700

    Change images in portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -23,6 +23,32 @@ const portfolio: React.SFC<portfolioProps> = () => {
       </div>
       <div className="portfolio-sub">
         <div className="portfolio-sub_title">
+          <img src="img/display-wino.png" alt="Wine House | Wine Cellar App"/>
+          <h2>Wine House</h2>
+          <p>
+            Created using the Node.js framework Express, "Wine House" is a wine cellar management app that features a user authentication system created for MongoDB using Mongoose ODM that allows a user to create a secure account and then search the Snooth.com API for wine, edit wine bottle information, and add information to a collection.
+          </p>
+          <div className="portfolio-btn_wrapper">
+            <a href="https://winoapp.herokuapp.com" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">Visit</button></a>
+            <a href="https://github.com/mvrad/wine-house" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">GitHub</button></a>
+          </div>
+        </div>
+      </div>
+      <div className="portfolio-sub">
+        <div className="portfolio-sub_title">
+          <img src="img/display-blog.png" alt="Matthew Conrad | Blog"/>
+          <h2>My Blog</h2>
+          <p>
+            Built in the PHP Laravel framework and with ButterCMS, this is my personal blog where I talk about web development, discoveries and breakthroughs in science and engineering, movie reviews, and pretty much anything I find interesting enough to write about.
+          </p>
+          <div className="portfolio-btn_wrapper">
+            <a href="https://mconradblog.herokuapp.com" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">Visit</button></a>
+            <a href="https://github.com/mvrad/blog" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">GitHub</button></a>
+          </div>
+        </div>
+      </div>
+      <div className="portfolio-sub">
+        <div className="portfolio-sub_title">
           <img src="img/display-kjconrad.png" alt="Kelly Conrad | Performer"/>
           <h2>kjconrad.com</h2>
           <p>
@@ -36,27 +62,27 @@ const portfolio: React.SFC<portfolioProps> = () => {
       </div>
       <div className="portfolio-sub">
         <div className="portfolio-sub_title">
-          <img src="img/display-blot.png" alt="Blot | Note-Taking App"/>
-          <h2>Blot</h2>
+          <img src="img/display-tictactoe.png" alt="Tic-Tac-Toe"/>
+          <h2>Tic-Tac-Toe</h2>
           <p>
-            Bootstrapped with Create React App and styled using Material-UI, Blot is a simple, easy-to-use note taking app that allows a user to create and save notes and lists.
+            A two-person tic-tac-toe game created in "vanilla" JavaScript.
           </p>
           <div className="portfolio-btn_wrapper">
-            <a href="https://blotapp.herokuapp.com" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">Visit</button></a>
-            <a href="https://github.com/mvrad/blot" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">GitHub</button></a>
+            <a href="https://mvrad.github.io/tic-tac-toe" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">Visit</button></a>
+            <a href="https://github.com/mvrad/tic-tac-toe/blob/master/js/app.js" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">GitHub</button></a>
           </div>
         </div>
       </div>
       <div className="portfolio-sub">
         <div className="portfolio-sub_title">
-          <img src="img/display-wino.png" alt="Wino | Wine Cellar App"/>
-          <h2>Wino</h2>
+          <img src="img/display-architect.png" alt="Anderson-Gage Architects"/>
+          <h2>Architecture Firm</h2>
           <p>
-            Created using the Node.js framework Express, "Wino" is a wine cellar management app that features a user authentication system created for MongoDB using Mongoose ODM that allows a user to create a secure account and then search the Snooth.com API for wine, edit wine bottle information, and add information to a collection.
+            A site for a fictional architecture firm. Created with the traditional web stack of HTML, CSS, and JavaScript.
           </p>
           <div className="portfolio-btn_wrapper">
-            <a href="https://winoapp.herokuapp.com" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">Visit</button></a>
-            <a href="https://github.com/mvrad/wino" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">GitHub</button></a>
+            <a href="https://mvrad.github.io/anderson-gage" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">Visit</button></a>
+            <a href="https://github.com/mvrad/anderson-gage" target="_blank" rel='noopener noreferrer'><button className="btn" type="button">GitHub</button></a>
           </div>
         </div>
       </div>
