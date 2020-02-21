136e1418b5f983c8dd46798f195ab4163e97c681
Matthew Conrad <matthew.conrad@outlook.com>
2020-02-20 19:21:51 -0800

    Remove AlphaAI.tsx and Thinkspace.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Sidebar from './components/Sidebar';
 import Welcome from './components/Welcome';
 import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
-import AlphaAI from './components/AlphaAI';
-import Thinkspace from './components/Thinkspace';
 
 const App: React.FC = () => {
   return (
@@ -20,8 +18,6 @@ const App: React.FC = () => {
           <Route exact path="/" component={Welcome} />
           <Route exact path="/skills" component={Skills} />
           <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/alpha" component={AlphaAI} />
-          <Route exact path="/thinkspace" component={Thinkspace} />
         </Switch>
       </BrowserRouter>
     </div>
