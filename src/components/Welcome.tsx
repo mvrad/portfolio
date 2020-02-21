0f699c210b4b4b8843fb5d0e60f0ab51d029d46f
Matthew Conrad <matthew.conrad@outlook.com>
2020-02-21 10:20:39 -0800

    Adjust sidebar nav with flexbox align-items

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -8,7 +8,7 @@ const Welcome: React.SFC<WelcomeProps> = (props) => {
       <div className="welcome-wrapper">
         <h1>Hi! &#128075; I'm Matt, a web developer based in Seattle.</h1>
         <h2>I utilize my background in graphic design and software engineering to build web sites and web applications according to principles of good design and web development best practices.</h2>
-        <h2>When I'm not at the computer, I like to play guitar, play basketball, or attend meetups in the area like SeattleJS.</h2>
+        <h2>When I'm not at the computer, I like to play guitar or attend tech meetups such as SeattleJS.</h2>
       </div>
     </div>
   );
